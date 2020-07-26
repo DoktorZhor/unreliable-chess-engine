7f2ae37adb676d91632794b5bd611f8d0109770e
DoktorZhor <tomas.dlabka@protonmail.com>
2020-07-27 00:40:40 +0200

    Add implementation of basic chessboard representation
    Contains 12 bitboards, one for each piece type. Calling the constructor w/o parameters creates chessboard in starting position. Allows printing to console in human readable way for debugging.
    
    TODO: add tests

--- src/main/scala/com/uce/board/Constants.scala
+++ src/main/scala/com/uce/board/Constants.scala
@@ -9,4 +9,19 @@ object Constants {
   val rank8 = new Bitboard(0xFF00000000000000L)
   val lightSquares = new Bitboard(0x55AA55AA55AA55AAL)
   val darkSquares = new Bitboard(0xAA55AA55AA55AA55L)
+
+  // Starting positions for pieces
+  val wpInit = new Bitboard(0xFF000000000000L)
+  val wrInit = new Bitboard(0x8100000000000000L)
+  val wnInit = new Bitboard(0x4200000000000000L)
+  val wbInit = new Bitboard(0x2400000000000000L)
+  val wqInit = new Bitboard(0x1000000000000000L)
+  val wkInit = new Bitboard(0x800000000000000L)
+
+  val bpInit = new Bitboard(0xFF00L)
+  val brInit = new Bitboard(0x81L)
+  val bnInit = new Bitboard(0x42L)
+  val bbInit = new Bitboard(0x24L)
+  val bqInit = new Bitboard(0x10L)
+  val bkInit = new Bitboard(0x8L)
 }
---
+++ src/main/scala/com/uce/board/PieceType.scala
@@ -0,0 +1,6 @@
+package com.uce.board
+
+object PieceType extends Enumeration {
+  type PieceType = Value
+  val WP, WR, WN, WB, WQ, WK, BP, BR, BN, BB, BQ, BK = Value
+}
