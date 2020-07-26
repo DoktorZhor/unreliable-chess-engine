a97fb749b5f3093d991ca2e75cea4f8c7ddaec3c
DoktorZhor <tomas.dlabka@protonmail.com>
2020-07-26 21:40:35 +0200

    Implement basic bitboard functionality
    For better type safety, dedicated Bitboard type was introduced with some basic capabilities:
      - scanning for ls1b
      - serialization, which returns list with indices of all active bits
      - printing of bitboard to console in human readable format

---
+++ src/test/scala/com/uce/board/BitboardSpec.scala
@@ -0,0 +1,29 @@
+package com.uce.board
+
+import com.uce.board.Bitboard.Bitboard
+import org.scalatest.flatspec.AnyFlatSpec
+
+class BitboardSpec extends AnyFlatSpec {
+
+  "Forward bitScan" should "return None for empty bitboard" in {
+    assert(new Bitboard(0L).bitScanForward === None)
+  }
+
+  "Forward bitScan" should "return indices of LS1b" in {
+    assert(Constants.fileA.bitScanForward === Some(0))
+    assert(Constants.fileH.bitScanForward === Some(7))
+    assert(Constants.rank1.bitScanForward === Some(0))
+    assert(Constants.rank8.bitScanForward === Some(56))
+    assert(Constants.lightSquares.bitScanForward === Some(1))
+    assert(Constants.darkSquares.bitScanForward === Some(0))
+  }
+
+  "Bitboard serialization" should "return list with indices of active bits" in {
+    assert(new Bitboard(0L).serialize === List.empty)
+    assert(Constants.fileA.serialize === List(0, 8, 16, 24, 32, 40, 48, 56))
+    assert(Constants.fileH.serialize === List(7, 15, 23, 31, 39, 47, 55, 63))
+    assert(Constants.rank1.serialize === List(0, 1, 2, 3, 4, 5, 6, 7))
+    assert(Constants.rank8.serialize === List(56, 57, 58, 59, 60, 61, 62, 63))
+  }
+
+}
